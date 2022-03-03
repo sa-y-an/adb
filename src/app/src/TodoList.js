bbb9ace51689366fdb3f7f8514162634d4a86ce7
sa-y-an <sayan.ee.nitd@gmail.com>
2022-03-04 02:51:44 +0530

    error handling

--- src/app/src/TodoList.js
+++ src/app/src/TodoList.js
@@ -4,21 +4,38 @@ import Todos from "./Todos";
 const TodoList = () => {
 
     const [todos, setTodos] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
-    useEffect( () => {
-        fetch(`http://localhost:8000/todos/`)
-        .then( (res) => {
+
+    useEffect(() => {
+        setTimeout(() => {
+          fetch('http://localhost:8000/todos/')
+          .then(res => {
+            if (!res.ok) { // error coming back from server
+              throw Error('could not fetch the data for that resource');
+            } 
             return res.json();
-        } )
-        .then( (data) => {
+          })
+          .then(data => {
+            setIsPending(false);
             setTodos(data);
-            // console.log(data);
-        })
-    }, [])
+            setError(null);
+          })
+          .catch(err => {
+            // auto catches network / connection error
+            setIsPending(false);
+            setError(err.message);
+          })
+        }, 0);
+      }, [])
+    
 
 
     return ( 
         <div className="content">
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
             { todos && <Todos todos={todos} title={"List of Todos"} />}
         </div>
      );
