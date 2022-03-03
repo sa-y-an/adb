281374e6d27a8e6cda137ebe8804dd209d0d8556
sa-y-an <sayan.ee.nitd@gmail.com>
2022-03-04 01:00:54 +0530

    backend created successfully

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -4,17 +4,38 @@ from rest_framework.response import Response
 from rest_framework import status
 import json, logging, os
 from pymongo import MongoClient
+from .serializers import TodoSerializer
+
 
 mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 db = MongoClient(mongo_uri)['test_db']
+collection  = db["test_db"]
+
+
+def isValid(data) :
+    if (list(data.keys()) != ["todo"]) :
+        return False
+    return True
 
 class TodoListView(APIView):
 
     def get(self, request):
         # Implement this method - return all todo items from db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        ret = []
+        data = collection.find({})
+        for res in data :
+            ret.append(res)
+        lst = TodoSerializer(ret, many=True)
+        return Response(lst.data, status=status.HTTP_200_OK)
         
     def post(self, request):
         # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        data = request.data
+        if( isValid(data) == False) :
+            return Response({"invalid form data"}, status= status.HTTP_400_BAD_REQUEST)
+        result = collection.insert_one(data)
+        if (result.acknowledged) : 
+            return Response({"sucess"}, status=status.HTTP_200_OK)
+        else :
+            return Response({"failed"}, status=status.HTTP_424_FAILED_DEPENDENCY)
 
